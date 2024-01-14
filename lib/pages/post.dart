20af5ca4b5ff996e94074e41044a018eabfdc4cd
yosefsahle <yosefsahle48@gmail.com>
2024-01-14 17:57:45 +0300

    Christ Like

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,40 +1,48 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:our_app_demo/widgets/post_card.dart';
 
-class PostPage extends StatelessWidget {
+class PostPage extends StatefulWidget {
   const PostPage({super.key});
 
   @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  List _items = [];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/sample.json');
+    final data = await json.decode(response);
+
+    setState(() {
+      _items = data["items"];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      primary: false,
-      padding: const EdgeInsets.all(20),
-      crossAxisSpacing: 50,
-      mainAxisSpacing: 20,
-      crossAxisCount: 1,
-      children: <Widget>[
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-        PostCard(),
-      ],
-    );
-    ;
+    readJson();
+    return ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (context, index) {
+          return Container(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  PostCard(
+                    title: _items[index]["title"],
+                    description: _items[index]["description"],
+                    image: _items[index]["image"],
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class PostCard extends StatelessWidget {
-  const PostCard({super.key});
+  final String title;
+  final String description;
+  final String image;
+
+  PostCard(
+      {required this.title, required this.description, required this.image});
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +17,15 @@ class PostCard extends StatelessWidget {
       child: Column(
         children: [
           Image.network(
-            'https://images.unsplash.com/photo-1623172959921-630212f71058?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3Bpcml0dWFsfGVufDB8fDB8fHww',
+            image,
             fit: BoxFit.cover,
           ),
           Padding(
-            padding: EdgeInsets.only(top: 30, right: 12, left: 12),
+            padding: EdgeInsets.only(top: 30, right: 12, left: 12, bottom: 15),
             child: Column(
-              children: [
-                Text('Title'),
-                Text('description if the pist that should be idented ')
-              ],
+              children: [Text(title), Text(description)],
             ),
-          )
+          ),
         ],
       ),
     );
