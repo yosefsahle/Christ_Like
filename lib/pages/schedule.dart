eecf98d3e0b0608b973bff6828cdbda87479e596
yosefsahle <yosefsahle48@gmail.com>
2024-01-18 21:51:27 +0300

    schedule page

--- lib/pages/schedule.dart
+++ lib/pages/schedule.dart
@@ -9,6 +9,15 @@ class SchedulePage extends StatefulWidget {
 
 class _SchedulePageState extends State<SchedulePage> {
   String dropdownValue = 'Personal Life';
+
+  bool display(String text) {
+    if (text == 'Personal Life') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,51 +25,141 @@ class _SchedulePageState extends State<SchedulePage> {
         child: Center(
           child: Column(
             children: [
+              const SizedBox(
+                height: 50,
+              ),
               const Text(
                 'Schedule',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
+              const Center(
+                child: Text(
+                  'You Can Schedule for various Counsoling and prrayer Reqs',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
               const SizedBox(
                 height: 40,
               ),
-              Row(
-                children: [
-                  const Text(
-                    'Services Type   : ',
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  DropdownButton<String>(
-                    value: dropdownValue,
-                    icon: const Icon(Icons.keyboard_double_arrow_down_rounded),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = newValue!;
-                      });
-                    },
-                    items: const [
-                      DropdownMenuItem<String>(
-                        value: 'Personal Life',
-                        child: Text('Personal Life'),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    const Text(
+                      'Services Type : ',
+                      style: TextStyle(fontSize: 22, color: Colors.blue),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    DropdownButton<String>(
+                      value: dropdownValue,
+                      icon: const Icon(
+                        Icons.keyboard_double_arrow_down_rounded,
+                        color: Colors.blue,
                       ),
-                      DropdownMenuItem<String>(
-                        value: 'Marrage Life',
-                        child: Text('Marrage Life'),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: const [
+                        DropdownMenuItem<String>(
+                          value: 'Personal Life',
+                          child: Text(
+                            'Personal Life',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Marrage Life',
+                          child: Text(
+                            'Marrage Life',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Ministry',
+                          child: Text(
+                            'Ministry',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Personal Prayer',
+                          child: Text(
+                            'Personal Prayer  ',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              // second Selection if the user selects Persona life
+              Visibility(
+                visible: display(dropdownValue),
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      const Text(
+                        'Services Type : ',
+                        style: TextStyle(fontSize: 22, color: Colors.blue),
                       ),
-                      DropdownMenuItem<String>(
-                        value: 'Ministry',
-                        child: Text('MInistry'),
+                      const SizedBox(
+                        width: 20,
                       ),
-                      DropdownMenuItem<String>(
-                        value: 'Personal Prayer',
-                        child: Text('Personal Prayer'),
+                      DropdownButton<String>(
+                        value: dropdownValue,
+                        icon: const Icon(
+                          Icons.keyboard_double_arrow_down_rounded,
+                          color: Colors.blue,
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownValue = newValue!;
+                          });
+                        },
+                        items: const [
+                          DropdownMenuItem<String>(
+                            value: 'Prayer',
+                            child: Text(
+                              'Prayer',
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: 'Marrage Life',
+                            child: Text(
+                              'Marrage Life',
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: 'Ministry',
+                            child: Text(
+                              'MInistry',
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: 'Personal Prayer',
+                            child: Text(
+                              'Personal Prayer  ',
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              )
             ],
           ),
         ));
