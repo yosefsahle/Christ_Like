2ed3c0dbd2ae4a49008bd92658bc6d4dca2f8a34
yosefsahle <yosefsahle48@gmail.com>
2024-01-19 05:37:32 +0300

    Log In Page

--- lib/pages/screens/Login.dart
+++ lib/pages/screens/Login.dart
@@ -11,130 +11,120 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 30),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.orange.shade900,
-            Colors.orange.shade800,
-            Colors.orange.shade400
-          ]),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(
-              height: 80,
-            ),
-            const Padding(
+        body: ListView(
+      children: [
+        Container(
+            width: double.infinity,
+            color: Colors.white,
+            child: Padding(
               padding: EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Log In",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
+                children: [
+                  const SizedBox(
+                    height: 30,
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "WelCome Back",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-                child: Container(
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(60),
-                      topLeft: Radius.circular(60))),
-              child: Padding(
-                padding: const EdgeInsets.all(30),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 60,
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Skip',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                  const Center(
+                    child: Image(
+                      image: NetworkImage(
+                          'https://img.freepik.com/free-vector/access-control-system-abstract-concept_335657-3180.jpg'),
+                      width: 150,
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10))
-                          ]),
+                  ),
+                  Center(
                       child: Column(
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: Colors.grey.shade200))),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                  hintText: 'Email or Phone',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none),
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.indigoAccent.shade400,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Welcome Back',
+                        style: TextStyle(
+                            color: Colors.indigoAccent.shade400, fontSize: 16),
+                      )
+                    ],
+                  )),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          width: 400,
+                          height: 60,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email or Phone',
+                              border: OutlineInputBorder(),
+                              label: Text('Email or Phone'),
                             ),
                           ),
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: Colors.grey.shade200))),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                  hintText: 'Password',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Forget Password',
-                          style: TextStyle(color: Colors.grey),
-                        )),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Container(
-                      height: 50,
-                      margin: EdgeInsets.symmetric(horizontal: 50),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.orange.shade900,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                    )
-                  ],
-                ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const SizedBox(
+                          width: 400,
+                          height: 60,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email or Phone',
+                              border: OutlineInputBorder(),
+                              label: Text('Email or Phone'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        Container(
+                          width: 300,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.indigoAccent.shade400,
+                              borderRadius: BorderRadius.circular(8)),
+                          child: TextButton.icon(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.login,
+                                color: Colors.white,
+                              ),
+                              label: const Text(
+                                'Log in',
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                                onPressed: () {},
+                                child: const Text('Forget Password?')),
+                            TextButton(
+                                onPressed: () {}, child: const Text('Sign Up')),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-            )),
-          ],
-        ),
-      ),
-    );
+            ))
+      ],
+    ));
   }
 }
