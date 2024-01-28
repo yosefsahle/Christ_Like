6f0efb1b6d695129e38670a6d34bc05a9965af6b
yosefsahle <yosefsahle48@gmail.com>
2024-01-28 15:52:14 +0300

    post Page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,102 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Text('hello'), Text('data')],
+      children: [
+        //STORIES
+        Padding(
+          padding: EdgeInsets.only(left: 4, right: 4, top: 6, bottom: 15),
+          child: SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 150,
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: const Center(
+                      child: Text('Daily Feeds'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    width: 150,
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: const Center(
+                      child: Text('Daily News'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    width: 150,
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: const Center(
+                      child: Text('Daily News 2'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    width: 150,
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: const Center(
+                      child: Text('Daily Feeds 2'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                ],
+              )),
+        ),
+
+        //Posts
+        Expanded(
+          child: ListView(
+            children: [
+              Container(
+                decoration: const BoxDecoration(color: Colors.grey),
+                height: 250,
+                child: const Center(
+                  child: Icon(Icons.play_circle_fill_rounded),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(5),
+                child: Center(
+                  child: Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                ),
+              ),
+              Container(
+                height: 80,
+                decoration: BoxDecoration(color: Colors.grey[800]),
+                child: const Row(
+                  children: [
+                    Icon(Icons.audiotrack_rounded, color: Colors.amber),
+                    Text('Play Audio Now')
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
