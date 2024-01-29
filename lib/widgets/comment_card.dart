163c37ae5c7138df8d3a1c24caed39e714f60571
yosefsahle <yosefsahle48@gmail.com>
2024-01-29 13:30:51 +0300

    comment card

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CommentCard extends StatelessWidget {
-  const CommentCard({super.key});
+  final String userProfile;
+  final String userName;
+  final String commentDate;
+  final String commentBody;
+
+  const CommentCard(
+      {super.key,
+      required this.userProfile,
+      required this.userName,
+      required this.commentDate,
+      required this.commentBody});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: 80,
+      child: Row(
+        children: [
+          Image.network(
+            userProfile,
+          ),
+          Column(
+            children: [Text(userName), Text(commentDate), Text(commentBody)],
+          )
+        ],
+      ),
+    );
   }
 }
