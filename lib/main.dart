4a4fc1bbf126f77d46fc87a078684f3c4e3445f8
yosefsahle <yosefsahle48@gmail.com>
2024-01-29 13:42:40 +0300

    comment card

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:our_app_demo/pages/screens/Login.dart';
-import 'package:our_app_demo/pages/screens/first_choose.dart';
-import 'package:our_app_demo/pages/screens/splash_screen.dart';
+import 'package:our_app_demo/app/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: SplashScreen(),
-      ),
-    );
-  }
 }
